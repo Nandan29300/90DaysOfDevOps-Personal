27ca2dca0f20d658fcdaaa07c9c80f81112dcbfd
Nandan P Aghera <agheranandanp@gmail.com>
2026-08-06 13:40:55 +0530

    Update main.py

--- day-31/sample-all-instructions/main.py
+++ day-31/sample-all-instructions/main.py
@@ -1,1 +1,8 @@
-print("Hello from a Dockerfile using all instructions!")
+import os
+
+with open("sample.txt") as f:
+    txt = f.read()
+
+print("Sample-all-instructions image!")
+print(f"Contents of sample.txt: {txt.strip()}")
+print("Working directory is:", os.getcwd())

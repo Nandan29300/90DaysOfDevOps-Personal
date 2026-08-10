1c2d8aab46c3743bc56a94cd69de1077f3220c39
Nandan P Aghera <agheranandanp@gmail.com>
2026-08-10 23:12:26 +0530

    Convert script to Flask app for Docker

--- day-31/sample-all-instructions/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open("sample.txt") as f:
-    txt = f.read()
-
-print("Sample-all-instructions image!")
-print(f"Contents of sample.txt: {txt.strip()}")
-print("Working directory is:", os.getcwd())
